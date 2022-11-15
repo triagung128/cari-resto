6374b58f09ac2b44bee1cfa899faaa3e76396c07
triagung128 <triagung128@gmail.com>
2022-11-15 08:18:49 +0700

    create styles

---
+++ lib/common/styles.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color primaryColor = Color(0xFFFFFFFF);
+const Color secondaryColor = Color(0xFF1BAC4B);
+
+final TextTheme myTextTheme = TextTheme(
+  headline1: GoogleFonts.urbanist(
+    fontSize: 92,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5,
+  ),
+  headline2: GoogleFonts.urbanist(
+    fontSize: 57,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5,
+  ),
+  headline3: GoogleFonts.urbanist(
+    fontSize: 46,
+    fontWeight: FontWeight.w400,
+  ),
+  headline4: GoogleFonts.urbanist(
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  headline5: GoogleFonts.urbanist(
+    fontSize: 23,
+    fontWeight: FontWeight.w400,
+  ),
+  headline6: GoogleFonts.urbanist(
+    fontSize: 19,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15,
+  ),
+  subtitle1: GoogleFonts.urbanist(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15,
+  ),
+  subtitle2: GoogleFonts.urbanist(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  ),
+  bodyText1: GoogleFonts.urbanist(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+  ),
+  bodyText2: GoogleFonts.urbanist(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  button: GoogleFonts.urbanist(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25,
+  ),
+  caption: GoogleFonts.urbanist(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4,
+  ),
+  overline: GoogleFonts.urbanist(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+  ),
+);
