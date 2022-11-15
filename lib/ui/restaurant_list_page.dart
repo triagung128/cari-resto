7c27f8e241fd46d60f459d3c2710d5996d0ccd15
triagung128 <triagung128@gmail.com>
2022-11-15 10:37:42 +0700

    create list page

---
+++ lib/ui/restaurant_list_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+import '../data/model/restaurant.dart';
+
+class RestaurantListPage extends StatelessWidget {
+  static const routeName = '/restaurant_list';
+
+  const RestaurantListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Restaurant App'),
+      ),
+      body: FutureBuilder<String>(
+        future: DefaultAssetBundle.of(context)
+            .loadString('assets/local_restaurant.json'),
+        builder: (context, snapshot) {
+          final List<Restaurant> restaurants = parseRestaurants(snapshot.data);
+
+          return ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            itemCount: restaurants.length,
+            itemBuilder: (context, index) {
+              final restaurant = restaurants[index];
+              return _itemList(context, restaurant);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _itemList(BuildContext context, Restaurant restaurant) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(24),
+            child: SizedBox(
+              height: 100,
+              width: 125,
+              child: Image.network(
+                restaurant.pictureId!,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.grey[400],
+                      ),
+                    );
+                  }
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Icon(
+                    Icons.broken_image,
+                    size: 100,
+                    color: Colors.grey[400],
+                  );
+                },
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 8),
+                Text(
+                  restaurant.name!,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle2!
+                      .copyWith(fontWeight: FontWeight.w500, fontSize: 18),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_pin,
+                      size: 18,
+                      color: Colors.grey[400],
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      restaurant.city!,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2!
+                          .copyWith(color: const Color(0xFF616161)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.star,
+                      size: 18,
+                      color: Colors.yellow,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      restaurant.rating.toString(),
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2!
+                          .copyWith(color: const Color(0xFF616161)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
